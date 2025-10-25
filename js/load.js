79bf71a150f7092528102a1561f2446bc1f00adb
GoNearMe <130382745+GoNearMe@users.noreply.github.com>
2025-10-25 22:40:14 +0300

    Update logo source in configuration

--- js/load.js
+++ js/load.js
@@ -8,7 +8,7 @@
   // ==============================================================
   const CFG = {
     brand: "ЛПГ — Форум",
-    logoSrc: "https://sc-lpg-forum.netlify.app/assets/Logo-DTpHmfKg.jpg",
+    logoSrc: "/assets/Logo.jpg",
     accent: "#3867d6",
     accent2: "#ff9f43",
     bg0: "#eef6fb",
